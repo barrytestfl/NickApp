3d591ebc2c6e66562b9082d7619e5815d9f97026
salman1992 <samadi.salman1992@gmail.com>
2022-05-20 05:09:05 +0430

    add SVG Module

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,6 +1,7 @@
 import react, { useContext } from "react";
 import { View, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Svg, { Path } from "react-native-svg";
 import styles from "./styles";
 import { Context as AuthContext } from "../../context/AuthContext";
 const Home = () => {
@@ -8,6 +9,15 @@ const Home = () => {
 
   return (
     <SafeAreaView style={styles.container}>
+      <View>
+        <Svg>
+          <Path
+            fill="#00cba9"
+            fillOpacity="0.98"
+            d="M0,96L1440,256L1440,0L0,0Z"
+          />
+        </Svg>
+      </View>
       <View style={{ flex: 1, alignContent: "center" }}>
         <Text>Home {state.email}</Text>
       </View>
